4395024a54d75e0725df61bcc2c162f0a277d3b1
Bonnie-Gamer864 <100964239+Bonnie-Gamer864@users.noreply.github.com>
2022-06-21 18:28:36 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+var canvas, bg;
+var tom, tomImg1, tomImg2;
+var jerry, jerryImg1, jerryImg2;
+
+function preload() {
+    
+    bg = loadImage("garden.png");
+    tomImg1= loadAnimation("tomOne.png");
+    tomImg2=loadAnimation("tomTwo.png", "tomThree.png");
+    tomImg3= loadAnimation("tomFour.png");
+    jerryImg1=loadAnimation("jerryOne.png");
+    jerryImg2= loadAnimation("jerryTwo.png", "jerryThree.png");
+    jerryImg3=loadAnimation("jerryFour.png");
+
+}
+
+function setup(){
+    
+    canvas = createCanvas(1000,800);
+
+    tom = createSprite(870, 600, 10, 10);
+    tom.addAnimation("tomAcostado", tomImg1);
+    tom.scale = 0.2;
+    tom.debug = false;
+
+    jerry = createSprite(200, 600, 10, 10);
+    jerry.addAnimation("jerryParado", jerryImg1);
+    jerry.scale = 0.15;
+    jerry.debug = false;
+
+}
+
+function draw() {
+
+    background(bg);
+
+    if(tom.x - jerry.x < (tom.width - jerry.width)/2){ 
+        
+        tom.velocityX = 0;
+        tom.addAnimation("tomParado", tomImg3);
+        tom.x = 300;
+        tom.scale = 0.2;
+        tom.changeAnimation("tomParado");
+        
+        jerry.addAnimation("jerryChecando", jerryImg3);
+        jerry.scale = 0.15;
+        jerry.changeAnimation("jerryChecando");
+    }  
+
+    keyPressed();
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+    if(keyCode === LEFT_ARROW){
+        
+        tom.velocityX = -3; 
+        tom.addAnimation("tomCaminando", tomImg2);
+        tom.changeAnimation("tomCaminando");
+        
+        jerry.addAnimation("jerryBurlando", jerryImg2);
+        jerry.frameDelay = 25;
+        jerry.changeAnimation("jerryBurlando");
+    }
+}
